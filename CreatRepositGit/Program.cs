4ea87e4644478d6d9bdbdc947d11d7dc1bc19584
Sandro Junqui <sandrojunqui@gmail.com>
2021-08-31 21:03:59 -0300

    Projeto iniciado, mensagem Ola Mundo

---
+++ CreatRepositGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreatRepositGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
